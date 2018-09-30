bfa0c212ac353d66e42d5a563e23fbcd434c141f
1DavidWriting <35464987+1DavidWriting@users.noreply.github.com>
2018-09-30 16:26:26 -0400

    Added some stubs
    added some stubs.  Called 2 of them from Main method in AlphaCare.java.

--- AlphaCare/src/alphacare/AlphaCare.java
+++ AlphaCare/src/alphacare/AlphaCare.java
@@ -7,7 +7,9 @@ public class AlphaCare {
 
     public static void main(String[] args) {
         
-        
+        PatientList patientList = new PatientList();
+        patientList.getList();
+        patientList.addPatient(23);
         
     }
     
--- AlphaCare/src/alphacare/PatientList.java
+++ AlphaCare/src/alphacare/PatientList.java
@@ -22,15 +22,45 @@ public class PatientList {
      * @return 
      */
     public ArrayList getList() {
-        return this.idList;        
+        //return this.idList;
+        //stub:
+        ArrayList stubList = new ArrayList <>();
+        stubList.add(1);
+        stubList.add(2);
+        return stubList;
     }
+    
+    
     
     /**
      * It allows to add a patient.
      * @param newID
      */
     public void addPatient(int newID) {
-        this.idList.add(newID);        
+        this.idList.add(newID);
+        //stub:
+        ArrayList stubList = new ArrayList <>();
+        stubList.add(1);
+        stubList.add(2);
+        stubList.add(newID);
+        System.out.println("Added patientId to list:" + newID);
+        System.out.println("Updated list:");
+        System.out.println(stubList);        
+    }
+
+    /**
+     * @return the listID
+     */
+    public int getListID() {
+        return listID;        
+    }
+
+    /**
+     * @param listID the listID to set
+     */
+    public void setListID(int listID) {
+        this.listID = listID;
+        System.out.println("List ID set to: " + listID);
     }
     
 }
