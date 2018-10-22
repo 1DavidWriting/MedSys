b557f337a437265827ce5f8b54dfde97f0144732
Ryan Feng <rfeng70@yahoo.com>
2018-10-21 23:50:48 -0400

    Got rid of comments from last project.

--- AlphaCare/src/alphacare/AlphaCare.java
+++ AlphaCare/src/alphacare/AlphaCare.java
@@ -6,10 +6,8 @@ public class AlphaCare {
 
 
     public static void main(String[] args) {
-        //System.out.println("Run App.java file in TestHarness module");
         
         LoginController loginController = new LoginController();
-        //PatientController controller = new PatientController();
         
         
     }
