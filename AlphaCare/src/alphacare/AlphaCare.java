f25d99ed77f3da66ff5bdbe2d4d230c1b1021e10
Ryan Feng <rfeng70@yahoo.com>
2018-10-21 23:57:04 -0400

    Added comments about login credentials

--- AlphaCare/src/alphacare/AlphaCare.java
+++ AlphaCare/src/alphacare/AlphaCare.java
@@ -6,6 +6,8 @@ public class AlphaCare {
 
 
     public static void main(String[] args) {
+        //username: js1
+        //password: easy1
         
         LoginController loginController = new LoginController();
         
